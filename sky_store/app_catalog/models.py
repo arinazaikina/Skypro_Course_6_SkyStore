0ef840d9a11fd8bf90081f58c1115cfb9a325bc2
arina <sav2405@gmail.com>
2023-05-03 00:44:24 +0400

    task 2_3

--- sky_store/app_catalog/models.py
+++ sky_store/app_catalog/models.py
@@ -1,7 +1,12 @@
 from django.db import models
 
+NULLABLE = {'blank': True, 'null': True}
+
 
 class Feedback(models.Model):
+    """
+    Модель, описывающая обратную связь
+    """
     name = models.CharField(max_length=100, verbose_name='Имя')
     phone = models.CharField(max_length=20, verbose_name='Телефон')
     message = models.TextField(verbose_name='Сообщение')
@@ -14,3 +19,44 @@ class Feedback(models.Model):
 
     def __str__(self):
         return f'Отзыв №{self.pk} от {self.name}'
+
+
+class Category(models.Model):
+    """
+    Модель, описывающая категорию товара
+    """
+    name = models.CharField(max_length=100, verbose_name='Название')
+    description = models.TextField(verbose_name='Описание', **NULLABLE)
+
+    class Meta:
+        db_table = 'category'
+        verbose_name = 'Категория товара'
+        verbose_name_plural = 'Категории товаров'
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    """
+    Модель, описывающая товар
+    """
+    name = models.CharField(max_length=100, verbose_name='Наименование')
+    description = models.TextField(verbose_name='Описание')
+    image = models.ImageField(
+        upload_to='products/', verbose_name='Изображение (превью)', default='products/default.png'
+    )
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='Категория')
+    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Цена')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Дата последнего изменения')
+
+    class Meta:
+        db_table = 'product'
+        verbose_name = 'Товар'
+        verbose_name_plural = 'Товары'
+        ordering = ['name']
+
+    def __str__(self):
+        return f"{self.name} - {self.category} - {self.price}"
--- sky_store/config/urls.py
+++ sky_store/config/urls.py
@@ -1,10 +1,14 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path, include
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('app_catalog.urls', namespace='app_catalog'))
 ]
 
-urlpatterns += staticfiles_urlpatterns()
+if settings.DEBUG:
+    urlpatterns += staticfiles_urlpatterns()
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
