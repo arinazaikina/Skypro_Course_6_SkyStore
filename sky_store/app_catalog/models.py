33c4b6ffeb66ef3893d3c1b8e50b6563f4569085
arina <sav2405@gmail.com>
2023-05-03 00:53:03 +0400

    task_4 rollback migration

--- sky_store/app_catalog/models.py
+++ sky_store/app_catalog/models.py
@@ -27,7 +27,6 @@ class Category(models.Model):
     """
     name = models.CharField(max_length=100, verbose_name='Название')
     description = models.TextField(verbose_name='Описание', **NULLABLE)
-    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
 
     class Meta:
         db_table = 'category'
