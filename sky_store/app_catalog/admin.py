34b421d6428b5fdbfd8ce5bed6d496da879b9f63
arina <sav2405@gmail.com>
2023-05-03 01:02:16 +0400

    task_5

--- sky_store/app_catalog/admin.py
+++ sky_store/app_catalog/admin.py
@@ -1,8 +1,23 @@
 from django.contrib import admin
 
-from .models import Feedback
+from .models import Feedback, Category, Product
+
 
 @admin.register(Feedback)
 class FeedbackAdmin(admin.ModelAdmin):
     list_display = ['pk', 'name', 'phone']
     list_display_links = ['pk', 'name']
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['pk', 'name']
+    list_display_links = ['pk', 'name']
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['pk', 'name', 'price', 'category']
+    list_display_links = ['pk', 'name']
+    search_fields = ['name', 'description']
+    list_filter = ['category']
